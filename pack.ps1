ac3996d3ed630b7f4b7656a09949b934e185d8ab
Luca Haneklau <git@luca.lsys.ac>
2025-12-16 22:21:02 +0100

    fix: model pack

--- pack.ps1
+++ pack.ps1
@@ -12,16 +12,7 @@ if (Test-Path "requirements.txt") {
 }
 
 # Remember to add any additional files, and change the name of the plugin
-$artifacts = "cn-plugin-gemma-embedding.py", "requirements.txt", "manifest.json", "__init__.py", 
-    "model\added_tokens.json",
-    "model\config.json",
-    "model\generation_config.json",
-    "model\special_tokens_map.json",
-    "model\tokenizer.json",
-    "model\tokenizer.model",
-    "model\tokenizer_config.json",
-    "model\onnx\model_fp16.onnx",
-    "model\onnx\model_fp16.onnx_data"
+$artifacts = "cn-plugin-gemma-embedding.py", "requirements.txt", "manifest.json", "__init__.py", "model"
 
 if (Test-Path "deps") {
     $artifacts += "deps"
--- pack.sh
+++ pack.sh
@@ -18,15 +18,7 @@ artifacts=(
     "cn-plugin-gemma-embedding.py"
     "requirements.txt"
     "manifest.json" "__init__.py"
-    "model/added_tokens.json"
-    "model/config.json"
-    "model/generation_config.json"
-    "model/special_tokens_map.json"
-    "model/tokenizer.json"
-    "model/tokenizer.model"
-    "model/tokenizer_config.json"
-    "model/onnx/model_fp16.onnx"
-    "model/onnx/model_fp16.onnx_data"
+    "model"
 )
 
 if [ -d "deps" ]; then
